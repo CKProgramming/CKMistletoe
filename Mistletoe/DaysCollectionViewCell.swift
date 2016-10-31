dd33091db5efe56a7cbbd8ff459bc1d21f88aeb6
Zach <zach.kobes@zymoent.com>
2016-10-30 21:41:37 -0500

    Added appicon set to assets and added the days collection view.

---
+++ Mistletoe/DaysCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  DaysCollectionViewCell.swift
+//  Mistletoe
+//
+//  Created by Zach on 10/30/16.
+//  Copyright © 2016 Zach. All rights reserved.
+//
+
+import UIKit
+
+class DaysCollectionViewCell: UICollectionViewCell {
+	
+	@IBOutlet weak var daysBackground: UIImageView!
+}
